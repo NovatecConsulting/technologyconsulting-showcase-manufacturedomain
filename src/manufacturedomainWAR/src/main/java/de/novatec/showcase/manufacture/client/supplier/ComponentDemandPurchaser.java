bc9fa847151b531f7f5045fc3c49b01c493c7173
sfrevel <3075075+sfrevel@users.noreply.github.com>
2020-01-27 12:33:29 +0100

    [refactoring] add check for replacement od JNDI properties with environment vasribales from open liberty server.env file

--- src/manufacturedomainWAR/src/main/java/de/novatec/showcase/manufacture/client/supplier/ComponentDemandPurchaser.java
+++ src/manufacturedomainWAR/src/main/java/de/novatec/showcase/manufacture/client/supplier/ComponentDemandPurchaser.java
@@ -32,7 +32,7 @@ public class ComponentDemandPurchaser {
 	private static final String JNDI_PROPERTY_SUPPLIERDOMAIN_PURCHASE_URL = "supplierdomain.purchase.url";
 	private static final String JNDI_PROPERTY_SUPPLIERDOMAIN_USERNAME = "supplierdomain.username";
 	private static final String JNDI_PROPERTY_SUPPLIERDOMAIN_PASSWORD = "supplierdomain.password";
-	private static final Logger log = LoggerFactory.getLogger(ComponentDemandPurchaser.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ComponentDemandPurchaser.class);
 	private String purchaseUrl;
 	private String username;
 	private String password;
@@ -49,11 +49,14 @@ public class ComponentDemandPurchaser {
 			username = (String) new InitialContext().lookup(JNDI_PROPERTY_SUPPLIERDOMAIN_USERNAME);
 			password = (String) new InitialContext().lookup(JNDI_PROPERTY_SUPPLIERDOMAIN_PASSWORD);
 		} catch (NamingException e) {
-			log.error("JNDI properties " + JNDI_PROPERTY_SUPPLIERDOMAIN_PURCHASE_URL + " or "
+			LOG.error("JNDI properties " + JNDI_PROPERTY_SUPPLIERDOMAIN_PURCHASE_URL + " or "
 					+ JNDI_PROPERTY_SUPPLIERDOMAIN_USERNAME + " or " + JNDI_PROPERTY_SUPPLIERDOMAIN_PASSWORD
 					+ " not found! Using system properties where possible!", e);
 			throw new ManufactureDomainNotConfiguredException("One or more JNDI properties for the manufacture domain is/are missing!");
-			//TODO add check if variables values start with ${env. so that there is no replacement in the server.xml done
+		}
+		if (validateJNDIProperty(purchaseUrl) || validateJNDIProperty(username) || validateJNDIProperty(password)) {
+			throw new ManufactureDomainNotConfiguredException(
+					"One or more JNDI properties for the manufacture domain are missing in the server.env file of open liberty!");
 		}
 	}
 
@@ -77,5 +80,10 @@ public class ComponentDemandPurchaser {
 	private static Builder asUser(Builder builder, String userName, String password) {
 		return builder.property(HttpAuthenticationFeature.HTTP_AUTHENTICATION_BASIC_USERNAME, userName)
 				.property(HttpAuthenticationFeature.HTTP_AUTHENTICATION_BASIC_PASSWORD, password);
+	}
+	
+	private boolean validateJNDIProperty(String value) {
+		LOG.info(value);
+		return value.startsWith("${env.");
 	}
 }
